ab6a57c806c4e5f95dffcbd162075493531b6415
Justin Jacob Sumaya <justinjacobsumaya@Justins-MacBook-Air.local>
2021-06-30 10:17:18 +0800

    Added navbar

--- resources/js/vue/App.vue
+++ resources/js/vue/App.vue
@@ -1,22 +1,6 @@
 <template>
     <div>
-        <b-navbar toggleable type="dark" variant="dark">
-
-            <b-navbar-toggle target="navbar-toggle-collapse">
-            <template #default="{ expanded }">
-                <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
-                <b-icon v-else icon="chevron-bar-down"></b-icon>
-            </template>
-            </b-navbar-toggle>
-
-            <b-collapse id="navbar-toggle-collapse" is-nav>
-            <b-navbar-nav class="ml-auto">
-                <b-nav-item href="#">Link 1</b-nav-item>
-                <b-nav-item href="#">Link 2</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
+        <Navbar/>
         <div class="container py-3">
             <Calendar/>
         </div>
@@ -26,11 +10,13 @@
 <script>
 
 import Calendar from './Calendar';
+import Navbar from './Navbar';
 
 export default {
     name: 'App',
     components: {
-        Calendar
+        Calendar,
+        Navbar
     }
 }
 </script>
