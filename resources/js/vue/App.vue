8216dbc2317fc3d85cd31ad0d08c95d1a9cf9b4e
Justin Jacob Sumaya <justinjacobsumaya@Justins-MacBook-Air.local>
2021-06-30 09:31:45 +0800

    Drop coluum seeder

--- resources/js/vue/App.vue
+++ resources/js/vue/App.vue
@@ -19,5 +19,7 @@ export default {
 </script>
 
 <style>
-
+    body {
+        background-color: #efefef !important;
+    }
 </style>
--- resources/js/vue/Calendar.vue
+++ resources/js/vue/Calendar.vue
@@ -33,5 +33,7 @@ export default {
 </script>
 
 <style scoped>
-    
+    .card-header {
+        background-color: #fff !important;
+    }
 </style>
